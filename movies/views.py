acf9790316d13ad9192b7ec6abb34d6bee3985bd
madhum48999 <madhum48999@gmail.com>
2025-07-22 11:58:00 +0530

    changes to html home page

--- movies/views.py
+++ movies/views.py
@@ -1,22 +1,22 @@
-from   django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Movie
 
 
 def movies(request):
     data = Movie.objects.all()  
-    return render(request, 'movies/movies.html',{'movies' : data})
+    return render(request, 'movies/movies.html', {'movies': data})
 
 def home(request):
-    return HttpResponse("Welcome to the Movies App")
+    return render(request, 'movies/home.html')
 
 def detail(request, id):
-    data = Movie.objects.get(pk =id)
+    data = Movie.objects.get(pk=id)
     return render(request, 'movies/details.html', {'movie': data})
 
 def add(request):
     title = request.POST.get('title')
-    year =request.POST.get('year')
+    year = request.POST.get('year')
     
     if title and year:
         movie = Movie(title=title, year=year)
