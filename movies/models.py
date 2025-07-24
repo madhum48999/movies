39722663d498cde0f04b9553a1c9e49e7b42daf9
madhum48999 <madhum48999@gmail.com>
2025-07-24 10:58:03 +0530

    changes to back to home page or cancel option

--- movies/models.py
+++ movies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
-    year = models.IntegerField(default=000)
+    year = models.IntegerField(default=1999)
 
     def __str__(self):
         return f'{self.title} from {self.year}'
